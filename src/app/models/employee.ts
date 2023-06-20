e5724849c561fe22c1c3e3f2f7898134914eda6c
EnkelaBexheti <94056789+EnkelaBexheti@users.noreply.github.com>
2023-06-20 21:21:49 +0200

    crud operation for Employee

---
+++ src/app/models/employee.ts
@@ -0,0 +1,11 @@
+export interface Employee {
+    id?: number;
+    name: string;
+    surname: string;
+    tel: string;
+    departmenId: 0,
+    department: {
+      id: 0,
+      name: string
+}
+}
